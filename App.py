67df82be6c5b65442b4f54e627b27c7fd0c10072
ChMatheisl <81020004+ChMatheisl@users.noreply.github.com>
2023-04-01 17:04:58 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.header('Hello World Pokern')
 df = pd.read_excel('Test Tabelle.xlsx')
-if st.button('Click'):
+col1, col2, col3 = st.columns([1,1,1])
+if col2.button('Click'):
     st.table(df)
-
