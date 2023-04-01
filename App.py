fd0fab83e112c3ab5cb9211a1fc36ad60d99aa16
ChMatheisl <81020004+ChMatheisl@users.noreply.github.com>
2023-04-01 23:52:36 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,16 +3,35 @@ import pandas as pd
 import datetime
 from streamlit_option_menu import option_menu
 #import gspread
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+sheet_url = st.secrets["private_gsheets_url"]
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 st.set_page_config(    
     page_title="Alexa, spiel Snake Jazz",
     page_icon="🎰",
     layout="wide",)
-
 choose = option_menu("Poker Tracking", ["Neues Spiel", "Scoreboard", "Visuals"],
                          icons=['house', 'camera fill', 'kanban'],
                          menu_icon="app-indicator", default_index=0,
                          orientation='horizontal')
+st.write(rows)
 
 # @st.cache_data(ttl=5)
 # def load_data(sheets_url):
@@ -35,13 +54,17 @@ if choose == "Neues Spiel":
     # Spieergbenis
     name = st.text_input('Wer bist du?', 'Niko')
     einzahlung = st.slider('Wie viel hast du eingezahlt?', 0, 50, 10)
+    datum = st.date_input(
+    "Wann?",
+    datetime.dateime.today())
     abgang = st.slider('Wie viel hast du am Ende mitgenommen?', 0, 50, 10)
     spieler_ergebnis = { 
         'Spieler': name,
         'Einzahlung': einzahlung,
         'Endstand': abgang,
-        'Datum': datetime.datetime.today()
+        'Datum': datum
         }
+   
 
     col1, col2, col3 = st.columns([1,1,1])
 
