82e942f5aa2e2a572f42fb75053939ad4b5a10fe
ChMatheisl <81020004+ChMatheisl@users.noreply.github.com>
2023-04-01 17:23:34 +0200

    Update App.py

--- App.py
+++ App.py
@@ -5,5 +5,10 @@ df = pd.read_excel('Test Tabelle.xlsx')
 col1, col2, col3 = st.columns([1,1,1])
 if col2.button('Click'):
     st.table(df)
+
+st.subheader('Wer bist du?') 
+df['Neuer col'] = 'Test lul'
+df.to_excel('Test Tabelle.xlsx')
+
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
