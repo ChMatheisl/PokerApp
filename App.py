4d1a4b91bb7988dd10d6d5d2a0a533a706120269
ChMatheisl <81020004+ChMatheisl@users.noreply.github.com>
2023-04-02 14:19:21 +0200

    Update App.py

--- App.py
+++ App.py
@@ -89,7 +89,7 @@ if choose == "Neues Spiel":
     col1, col2, col3 = st.columns([1,1,1])
 
     if col1.button('Abschicken'):
-        query = f'INSERT INTO "%(sheet_url)s" SELECT %(name)s, %(einzahlung)s, %(abgang)s, %(datum)s'
+        query = 'INSERT INTO "%(sheet_url)s" SELECT %(name)s, %(einzahlung)s, %(abgang)s, %(datum)s'
         parameter = {'sheet_url': sheet_url, 'name': name, 'einzahlung': einzahlung, 'abgang': abgang, 'datum': datum }
         #query = f'INSERT INTO "{sheet_url}" SELECT {name}, {einzahlung}, {abgang}, {datum}'
         cursor.execute(query, parameter)
