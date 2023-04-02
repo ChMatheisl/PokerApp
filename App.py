70595f0b74a79a5e97c4902fce4528d92d2cac5f
ChMatheisl <81020004+ChMatheisl@users.noreply.github.com>
2023-04-02 14:29:42 +0200

    Update App.py

--- App.py
+++ App.py
@@ -89,8 +89,8 @@ if choose == "Neues Spiel":
     col1, col2, col3 = st.columns([1,1,1])
 
     if col1.button('Abschicken'):
-        query = 'INSERT INTO ? SELECT ?, ?, ?, ?'
-        parameter = {'sheet_url': sheet_url, 'name': name, 'einzahlung': einzahlung, 'abgang': abgang, 'datum': datum}
+        query = f'INSERT INTO {sheet_url} SELECT (?, ?, ?, ?)'
+        parameter = {'name': name, 'einzahlung': einzahlung, 'abgang': abgang, 'datum': datum}
         #query = f'INSERT INTO "{sheet_url}" SELECT {name}, {einzahlung}, {abgang}, {datum}'
         cursor.execute(query, tuple(parameter.values()))
         #st.session_state['df'] = pd.concat([st.session_state['df'], pd.DataFrame.from_records([spieler_ergebnis])], ignore_index=True)
